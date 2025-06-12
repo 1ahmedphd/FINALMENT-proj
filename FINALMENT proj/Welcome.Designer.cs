ab3d5652fd670aff2565b5f28a528819550e2974
SarufOS <sarufmuhammadh@gmail.com>
2025-06-12 15:56:13 +0800

    baby steps

--- FINALMENT proj/Welcome.Designer.cs
+++ FINALMENT proj/Welcome.Designer.cs
@@ -137,12 +137,12 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40.1F);
-            this.label1.Location = new System.Drawing.Point(104, 25);
+            this.label1.Location = new System.Drawing.Point(252, 32);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(426, 76);
+            this.label1.Size = new System.Drawing.Size(141, 76);
             this.label1.TabIndex = 0;
-            this.label1.Text = "SedapMakan";
+            this.label1.Text = "test";
             // 
             // tabPage2
             // 
